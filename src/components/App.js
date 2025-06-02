4baa6d336a553705a06cda4fb9210412cde1746c
chadwin-uhuru <uhuruchadwin166@gmail.com>
2025-06-02 21:47:30 +0300

    Complete React Controlled Components Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,52 @@
 import React, { useState } from "react";
-import ShoppingList from "./ShoppingList";
-import Header from "./Header";
-import itemData from "../data/items";
+import ItemForm from "./ItemForm";
+import Filter from "./Filter";
+import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function App() {
-  const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [items, setItems] = useState([
+    { id: "1", name: "Apple", category: "Produce" },
+    { id: "2", name: "Milk", category: "Dairy" },
+    { id: "3", name: "Cake", category: "Dessert" },
+  ]);
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+  const [search, setSearch] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleSearchChange(searchText) {
+    setSearch(searchText);
   }
 
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+
+  function handleItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    const matchesCategory =
+      selectedCategory === "All" || item.category === selectedCategory;
+    const matchesSearch = item.name.toLowerCase().includes(search.toLowerCase());
+    return matchesCategory && matchesSearch;
+  });
+
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+    <div className="App">
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <Filter
+        search={search}
+        onSearchChange={handleSearchChange}
+        selectedCategory={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+      />
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+      </ul>
     </div>
   );
 }
